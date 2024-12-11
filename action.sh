d74dec0a6bcbbdd4394fbc1936cee1463746d5a9
TheWildJames <bins4us@hotmail.com>
2024-12-11 06:44:36 -0500

    Update Module

--- action.sh
+++ action.sh
@@ -4,9 +4,11 @@ MODDIR="${0%/*}"
 desription="Module for Wild Kernel"
 kernel_version=$(cat /proc/version)
 
-if echo "$kernel_version" | grep -q "wild"; then
-	desription="description=Wild Kernel | status: active ✅"
+if echo "$kernel_version" | grep -q "ild"; then
+	echo "You are using Wild Kernel"
+    desription="description=Wild Kernel | status: active ✅"
 else
+    echo "You are not using Wild Kernel"
     desription="description=Wild Kernel | status: disabled ❌"
 fi
 
